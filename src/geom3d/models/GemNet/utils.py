5820b1756d3d098b1b09dc89f6a36e3e4ee7a4f7
cyprienbone <141357631+cyprienbone@users.noreply.github.com>
2023-12-08 18:51:29 +0000

    Added GemNet, SchNet
    Added files for new models to explore but they still dont work. Also did the inference tests for the first 3 models SchNet, DimeNet and Dimenet++

--- src/geom3d/models/GemNet/utils.py
+++ src/geom3d/models/GemNet/utils.py
@@ -2,13 +2,25 @@ import json
 
 
 def read_json(path):
+    # """ """
+    # if not path.endswith(".json"):
+    #     raise UserWarning(f"Path {path} is not a json-path.")
+
+    # with open(path, "r") as f:
+    #    content = json.load(f)
+    # return content
     """ """
+    if path is None:
+        return None  # or raise an error, depending on your requirements
     if not path.endswith(".json"):
         raise UserWarning(f"Path {path} is not a json-path.")
+    try:
+        with open(path, "r") as f:
+            content = json.load(f)
+        return content
+    except FileNotFoundError:
+        return None  # or handle the absence of the file in an appropriate way
 
-    with open(path, "r") as f:
-        content = json.load(f)
-    return content
 
 
 def update_json(path, data):
@@ -34,7 +46,14 @@ def read_value_json(path, key):
     """ """
     content = read_json(path)
 
-    if key in content.keys():
-        return content[key]
+    # if key in content.keys():
+    #     return content[key]
+    # else:
+    #     return None
+    if content is not None:
+        if key in content.keys():
+            return content[key]
+        else:
+            return None
     else:
         return None
